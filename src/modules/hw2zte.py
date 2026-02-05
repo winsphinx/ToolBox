0234bb871d9ef74dc0d94b51e55deaee10b422fc
winsphinx <winsphinx@gmail.com>
2026-02-05 10:14:19 +0800

    separate ipv4/6

--- src/modules/hw2zte.py
+++ src/modules/hw2zte.py
@@ -30,10 +30,10 @@ class Hw2Zx:
 
     @use_scope("output", clear=True)
     def update(self):
+        content = ""
+        params = []
         code = pin["code"]
         segments = re.findall(r"(\ninterface Eth-Trunk.*?\n#)", code, re.DOTALL)
-
-        params = []
         for segment in segments:
             desc_match = re.search(r"description (.+)", segment)
             vid_match = re.search(r"interface Eth-Trunk([\d.]+)", segment)
@@ -50,7 +50,7 @@ class Hw2Zx:
                     "description": desc_match.group(1).strip() if desc_match else "no description",
                     "ipv4": ipv4_match.group(1),
                     "mask": ipv4_match.group(2),
-                    "ipv6": ipv6_match.group(1) if ipv6_match else "",
+                    "ipv6": ipv6_match.group(1) if ipv6_match else None,
                     "policy": policy_match.group(1) if policy_match else "NoWebNoDNS",
                     "dot1q": dot1q_match.group(1) if dot1q_match else None,
                     "qinq_pv": qinq_match.group(1) if qinq_match else None,
@@ -58,36 +58,36 @@ class Hw2Zx:
                 }
                 params.append(param)
 
-        content = ""
-
         for item in params:
             if item["dot1q"]:
-                block = f"""interface smartgroup1.{item["vid"]}
+                content += f"""\n\ninterface smartgroup1.{item["vid"]}
  description {item["description"]}
-  ipv6 enable
   ip address {item["ipv4"]} {item["mask"]}
-  ipv6 address {item["ipv6"]}
   encapsulation-dot1q {item["vid"]}
   ipv4-access-group egress {item["policy"]}
-  ipv6-access-group egress {item["policy"]}
   ipv4 verify unicast source reachable-via any ignore-default-route
+"""
+                if item["ipv6"]:
+                    content += f"""  ipv6 enable
+  ipv6 address {item["ipv6"]}
+  ipv6-access-group egress {item["policy"]}
   ipv6 verify unicast source reachable-via any ignore-default-route
 """
-                content += block + "\n\n"
 
             elif item["qinq_cv"]:
-                block = f"""interface smartgroup1.{item["vid"]}
+                content += f"""\n\ninterface smartgroup1.{item["vid"]}
  description {item["description"]}
-  ipv6 enable
   ip address {item["ipv4"]} {item["mask"]}
-  ipv6 address {item["ipv6"]}
   qinq internal-vlanid {item["qinq_pv"]} external-vlanid {item["qinq_cv"]}
   ipv4-access-group egress {item["policy"]}
-  ipv6-access-group egress {item["policy"]}
   ipv4 verify unicast source reachable-via any ignore-default-route
+"""
+                if item["ipv6"]:
+                    content += f"""  ipv6 enable
+  ipv6 address {item["ipv6"]}
+  ipv6-access-group egress {item["policy"]}
   ipv6 verify unicast source reachable-via any ignore-default-route
 """
-                content += block + "\n\n"
 
         put_markdown(content)
 
