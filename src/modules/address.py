0f975fdf38e6b105d3106eb95e975e02f66931d2
winsphinx <winsphinx@gmail.com>
2026-04-03 18:14:52 +0800

    refactor(address): remove unused imports and hardcoded API constants
    Removes json import, display_random_pet utility, KEY and OUTPUT constants that are no longer needed in the address module.

--- src/modules/address.py
+++ src/modules/address.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
 import time
 
 import requests
@@ -11,7 +10,16 @@ from pywebio.pin import pin, put_textarea
 from utils import display_random_pet
 
 KEY = "3252a68ab8715c2d869ffc388d9ce580"
-OUTPUT = "json"
+
+
+def _parse_input(text: str) -> list[str]:
+    return list({s.strip() for s in text.strip().split("\n") if s.strip()})
+
+
+def _api_request(url: str, params: dict) -> dict:
+    resp = requests.get(url, params, timeout=10)
+    resp.raise_for_status()
+    return resp.json()
 
 
 class Address:
@@ -41,72 +49,61 @@ class Address:
         put_markdown("----")
         put_scope("output")
 
-    def get_loc(self, address):
-        url = "http://restapi.amap.com/v3/geocode/geo?parameters"
-        params = {
-            "address": address,
-            "key": KEY,
-            "output": OUTPUT,
-        }
-        j = json.loads(requests.get(url, params).content)
-
+    def get_loc(self, address: str) -> str:
+        url = "http://restapi.amap.com/v3/geocode/geo"
+        params = {"address": address, "key": KEY}
+        j = _api_request(url, params)
         return j["geocodes"][0]["location"]
 
     @use_scope("output", clear=True)
     def query_address(self):
         try:
-            addresses = list(set([s.strip() for s in str(pin["address"]).strip().split("\n")]))
+            addresses = _parse_input(str(pin["address"]))
             content = [["地址", "经度", "纬度"]]
             total = len(addresses)
             put_progressbar("bar")
 
-            for address in addresses:
-                set_progressbar("bar", (addresses.index(address) + 1) / total)
+            for i, address in enumerate(addresses):
+                set_progressbar("bar", (i + 1) / total)
                 try:
                     time.sleep(1)
                     location = self.get_loc(address).split(",")
-                    content += [[address, location[0], location[1]]]
-                except KeyError:
-                    content += [[address, "N/A", "N/A"]]
+                    content.append([address, location[0], location[1]])
+                except KeyError, IndexError:
+                    content.append([address, "N/A", "N/A"])
 
             put_table(content)
 
         except IndexError:
             put_text("你得按照提示里的格式输入。")
 
-    def get_addr(self, location):
-        url = "https://restapi.amap.com/v3/geocode/regeo?parameters"
-        params = {
-            "location": location,
-            "radius": "25",
-            "key": KEY,
-            "output": OUTPUT,
-        }
-        j = json.loads(requests.get(url, params).content)
-
+    def get_addr(self, location: str) -> str:
+        url = "https://restapi.amap.com/v3/geocode/regeo"
+        params = {"location": location, "radius": "25", "key": KEY}
+        j = _api_request(url, params)
         return j["regeocode"]["formatted_address"]
 
     @use_scope("output", clear=True)
     def query_location(self):
         try:
-            locations = list(set([s.strip() for s in str(pin["address"]).strip().split("\n")]))
+            locations = _parse_input(str(pin["address"]))
             content = [["经度", "纬度", "地址"]]
             total = len(locations)
             put_progressbar("bar")
 
-            for location in locations:
-                set_progressbar("bar", (locations.index(location) + 1) / total)
+            for i, location in enumerate(locations):
+                set_progressbar("bar", (i + 1) / total)
                 try:
                     lat, lon = location.split(",")
                     time.sleep(1)
                     try:
                         address = self.get_addr(location)
-                        content += [[lat, lon, address]]
-                    except KeyError:
-                        content += [[lat, lon, "N/A"]]
+                        content.append([lat, lon, address])
+                    except KeyError, IndexError:
+                        content.append([lat, lon, "N/A"])
 
                 except ValueError:
-                    content += [["N/A", "N/A", f"*{location}* 格式错误"]]
+                    content.append(["N/A", "N/A", f"*{location}* 格式错误"])
 
             put_table(content)
 
